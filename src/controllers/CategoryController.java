d4361dc3c60e57b8dc7d5fd7e4f644cd775b6475
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-03-07 17:18:00 +0100

    Dodanie pustych widokow i kontrolerow

---
+++ src/controllers/CategoryController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class CategoryController  implements Initializable  {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		System.out.println("Inicjacja Kategori");
+		
+	}
+
+}
