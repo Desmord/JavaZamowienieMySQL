d79492e5bef0191247df785cda934df188a244f8
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-03-11 17:03:04 +0100

    Dodanie zmiennych oznaczajacych pierwsze wczytanie w ApplicationData

--- src/application/ApplicationData.java
+++ src/application/ApplicationData.java
@@ -7,6 +7,11 @@ public class ApplicationData {
 	private final static String DB_PATH = "jdbc:mysql://localhost:3307/zamowienia";
 	private final static String USER = "sklepikarz";
 	private final static String PASSWORD = "haselko";
+	
+	private static boolean isOrderLoaded = false;
+	private static boolean isCustomerLoaded = false;
+	private static boolean isCategoryLoaded = false;
+	private static boolean isProductLoaded = false;
 
 	private ApplicationData() {
 	}
@@ -39,6 +44,38 @@ public class ApplicationData {
 
 	public static void setInstance(ApplicationData instance) {
 		ApplicationData.instance = instance;
+	}
+
+	public static boolean isOrderLoaded() {
+		return isOrderLoaded;
+	}
+
+	public static void setOrderLoaded(boolean isOrderLoaded) {
+		ApplicationData.isOrderLoaded = isOrderLoaded;
+	}
+
+	public static boolean isCustomerLoaded() {
+		return isCustomerLoaded;
+	}
+
+	public static void setCustomerLoaded(boolean isCustomerLoaded) {
+		ApplicationData.isCustomerLoaded = isCustomerLoaded;
+	}
+
+	public static boolean isCategoryLoaded() {
+		return isCategoryLoaded;
+	}
+
+	public static void setCategoryLoaded(boolean isCategoryLoaded) {
+		ApplicationData.isCategoryLoaded = isCategoryLoaded;
+	}
+
+	public static boolean isProductLoaded() {
+		return isProductLoaded;
+	}
+
+	public static void setProductLoaded(boolean isProductLoaded) {
+		ApplicationData.isProductLoaded = isProductLoaded;
 	}
 
 }
