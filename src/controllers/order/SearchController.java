b686712780ff66d0841a27e6339190224570b933
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-03-14 21:39:33 +0100

    Stworzenie widoku wyszukiwania, dodanie zdarzenia klikniecia przycisku wyszukania

--- src/controllers/order/SearchController.java
+++ src/controllers/order/SearchController.java
@@ -3,27 +3,61 @@ package controllers.order;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class SearchController  implements Initializable{
+public class SearchController implements Initializable {
 
-	
-    @FXML
-    private AnchorPane mainAnchorPane;
+	@FXML
+	private AnchorPane mainAnchorPane;
 
-	
+	@FXML
+	private CheckBox orderNumberCheckBox;
+
+	@FXML
+	private CheckBox customerIdCheckBox;
+
+	@FXML
+	private TextField searchTextField;
+
+	@FXML
+	private Button searchButton;
+
+	@FXML
+	private Label infoLabel;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		System.out.println("Inicjacja szukania Zamowienia");
+
+		setSearchButtonClickEvent();
+		
 	}
 
 	public void displayPane(boolean display) {
 
 		mainAnchorPane.setVisible(display);
 
+	}
+
+	private void setSearchButtonClickEvent() {
+		
+		searchButton.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				
+				System.out.println("aksdhg");
+				
+			}
+		});
+		
 	}
 	
 }
