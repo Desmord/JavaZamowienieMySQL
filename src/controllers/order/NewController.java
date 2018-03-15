3b3926ba036a2958fea87d263d5abb099362435e
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-03-15 21:23:39 +0100

    Stworzenie widoku dodawania nowego zamowienia

--- src/controllers/order/NewController.java
+++ src/controllers/order/NewController.java
@@ -3,8 +3,13 @@ package controllers.order;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class NewController implements Initializable {
@@ -12,16 +17,43 @@ public class NewController implements Initializable {
 	@FXML
 	private AnchorPane mainAnchorPane;
 
+	@FXML
+	private TextField customerNameTextField;
+
+	@FXML
+	private TextField discountTextField;
+
+	@FXML
+	private Button addButton;
+
+	@FXML
+	private Label infoLabel;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
-		System.out.println("Inicjacja nowy produkt pane");
+		setAddButtonClickEvent();
 
 	}
 
 	public void displayPane(boolean display) {
 
 		mainAnchorPane.setVisible(display);
+		mainAnchorPane.toFront();
+
+	}
+
+	private void setAddButtonClickEvent() {
+
+		addButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+
+				System.out.println("Witaj swiece");
+
+			}
+		});
 
 	}
 
