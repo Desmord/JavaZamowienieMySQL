af4217a527d1316904fbf6442e5d1a39466ff4dd
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-03-15 10:34:55 +0100

    Dodanie zdarzen do checkboxow, sprawdzanie poprawnosci danych

--- src/controllers/order/SearchController.java
+++ src/controllers/order/SearchController.java
@@ -3,6 +3,7 @@ package controllers.order;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import DAO.OrderDao;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -36,8 +37,10 @@ public class SearchController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
+		setOrderNumberCheckBoxCheckEvent();
+		setCustomerIdCheckBoxCheckEvent();
 		setSearchButtonClickEvent();
-		
+
 	}
 
 	public void displayPane(boolean display) {
@@ -46,18 +49,102 @@ public class SearchController implements Initializable {
 
 	}
 
-	private void setSearchButtonClickEvent() {
-		
-		searchButton.setOnAction(new EventHandler<ActionEvent>() {
-			
+	private void setOrderNumberCheckBoxCheckEvent() {
+
+		orderNumberCheckBox.setOnAction(new EventHandler<ActionEvent>() {
+
 			@Override
 			public void handle(ActionEvent arg0) {
-				
-				System.out.println("aksdhg");
-				
+
+				customerIdCheckBox.setSelected(false);
+
 			}
 		});
-		
+
 	}
-	
+
+	private void setCustomerIdCheckBoxCheckEvent() {
+
+		customerIdCheckBox.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+
+				orderNumberCheckBox.setSelected(false);
+
+			}
+		});
+
+	}
+
+	private void setSearchButtonClickEvent() {
+
+		searchButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+
+				infoLabel.setText("");
+				
+				if (isTextFieldEmpty()) {
+
+					if (orderNumberCheckBox.isSelected()) {
+
+						if (isNumber()) {
+
+							System.out.println("id");
+
+						}else {
+							
+							infoLabel.setText("Muszisz podaæ liczbê.");
+							
+						}
+
+					} else if (customerIdCheckBox.isSelected()) {
+
+						System.out.println("klient");
+
+					} else {
+
+						infoLabel.setText("Nie wybrano opcji wyszukiwania.");
+
+					}
+
+				} else {
+
+					infoLabel.setText("Nie wpisano ¿adnej wartoœci w pole tekstowe.");
+
+				}
+
+			}
+		});
+
+	}
+
+	private boolean isTextFieldEmpty() {
+
+		if (searchTextField.getText().equals("")) {
+
+			return false;
+
+		}
+
+		return true;
+	}
+
+	private boolean isNumber() {
+
+		String text = searchTextField.getText();
+		
+		for(int i = 0;i<text.length();i++) {
+		
+			if(!text.substring(i,i+1).matches("[0-9]")) {
+				return false;
+			}
+			
+		}
+		
+		return true;
+	}
+
 }
