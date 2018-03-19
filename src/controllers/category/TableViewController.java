fb08b9d3a12b121f43cd8f718a02aae330a8c50b
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-03-19 20:48:03 +0100

    Dodanie tabeli do widoku Kategori

--- src/controllers/category/TableViewController.java
+++ src/controllers/category/TableViewController.java
@@ -3,14 +3,22 @@ package controllers.category;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import dataClasess.CategoryData;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.AnchorPane;
 
 public class TableViewController implements Initializable  {
 	
 	@FXML
 	private AnchorPane mainAnchorPane;
+
+    @FXML
+    private TableColumn<CategoryData, String> idTableCell;
+
+    @FXML
+    private TableColumn<CategoryData, String> nameTableCell;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
