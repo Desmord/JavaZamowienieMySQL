1468692d12e7711a56e6837332e18418bd90b26e
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-03-22 18:10:06 +0100

    Edycja wygladu Tabeli z produktami i dodanie do niej nowych kolumn

--- src/controllers/product/TableController.java
+++ src/controllers/product/TableController.java
@@ -5,6 +5,8 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
 public class TableController implements Initializable {
@@ -13,6 +15,27 @@ public class TableController implements Initializable {
 
 	@FXML
 	private AnchorPane mainAnchorPane;
+
+	@FXML
+	private TableView<?> mainTableView;
+
+	@FXML
+	private TableColumn<?, ?> idCell;
+
+	@FXML
+	private TableColumn<?, ?> nameCell;
+
+	@FXML
+	private TableColumn<?, ?> priceCell;
+
+	@FXML
+	private TableColumn<?, ?> quantityCell;
+
+	@FXML
+	private TableColumn<?, ?> discountCell;
+
+	@FXML
+	private TableColumn<?, ?> categoryCell;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
