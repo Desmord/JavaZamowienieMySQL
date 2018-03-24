fa4d31bdab9ca9c4aecf8a2d52eb5ee86cb16518
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-03-24 16:34:20 +0100

    Dodanie zdarzen klikniecia do elementow panelu wyszukaj katyegori Produkty

--- src/controllers/product/SearchController.java
+++ src/controllers/product/SearchController.java
@@ -3,6 +3,8 @@ package controllers.product;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -16,33 +18,120 @@ public class SearchController implements Initializable {
 	ProductController productController;
 
 	@FXML
-    private AnchorPane mainAnchorPane;
+	private AnchorPane mainAnchorPane;
 
-    @FXML
-    private CheckBox idNumberCheckBox;
+	@FXML
+	private CheckBox idNumberCheckBox;
 
-    @FXML
-    private CheckBox nameCheckBox;
+	@FXML
+	private CheckBox nameCheckBox;
 
-    @FXML
-    private CheckBox categoryCheckBox;
+	@FXML
+	private CheckBox categoryCheckBox;
 
-    @FXML
-    private TextField mainTextField;
+	@FXML
+	private TextField mainTextField;
 
-    @FXML
-    private Button showCategoryButton;
+	@FXML
+	private Button showCategoryButton;
 
-    @FXML
-    private Button searchButton;
+	@FXML
+	private Button searchButton;
 
-    @FXML
-    private Label infoLabel;
+	@FXML
+	private Label infoLabel;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		System.out.println("Inicjacja kontroleara wyszukiania produktu");
+		setElementsClickEvents();
+	}
+
+	private void setElementsClickEvents() {
+		setIdNumberChcekBoxClickEvent();
+		setNameCheckBoxClickEvent();
+		setCategoryChcekBoxClickEvent();
+		setSearchButtonClickEvent();
+		setShowCategoryButtonClickEvent();
+	}
+	
+	private void setSearchButtonClickEvent() {
+		searchButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {				
+				infoLabel.setText("");
+				
+				if(!mainTextField.getText().equals("")) {				
+					if(idNumberCheckBox.isSelected()) {
+						searchId();
+					}else if(nameCheckBox.isSelected()) {
+						searchName();
+					}else if(categoryCheckBox.isSelected()) {
+						searchCategory();
+					}else {
+						infoLabel.setText("Zaznacz opcjê wyszukiwania.");
+					}	
+				}else {
+					infoLabel.setText("Wpisz wartoœæ do wyszukania.");
+				}			
+			}
+		});
+	}
+	
+	private void searchId() {
+		System.out.println("id");
+	}
+	
+	private void searchName() {
+		System.out.println("nazwa");
+	}
+	
+	private void searchCategory() {
+		System.out.println("kategori");
+	}
+
+	private void setShowCategoryButtonClickEvent() {
+		showCategoryButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				System.out.println("pokaz kateogri");
+				
+			}
+		});
+	}
+	
+	private void setCategoryChcekBoxClickEvent() {
+		categoryCheckBox.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {							
+				nameCheckBox.setSelected(false);
+				idNumberCheckBox.setSelected(false);
+			}
+		});
+	}
+	
+	private void setNameCheckBoxClickEvent() {
+		nameCheckBox.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				categoryCheckBox.setSelected(false);
+				idNumberCheckBox.setSelected(false);
+			}
+		});
+	}
+
+	private void setIdNumberChcekBoxClickEvent() {
+		idNumberCheckBox.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				nameCheckBox.setSelected(false);
+				categoryCheckBox.setSelected(false);
+			}
+		});
 	}
 
 	public void setMainProductController(ProductController productController) {
